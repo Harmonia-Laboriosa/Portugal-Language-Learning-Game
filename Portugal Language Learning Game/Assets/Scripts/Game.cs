6e6620b8bb37228f891991568f43a41d6814b669
Akshali Jain <jainakshali03@gmail.com>
2024-02-06 12:23:31 +0530

    commit

---
+++ Portugal Language Learning Game/Assets/Scripts/Game.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+public class Game : MonoBehaviour
+{
+    public TMP_Text ScoreText;
+    public GameObject CurrentPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentPlayer = GameObject.FindGameObjectWithTag("CurrentPlayer");
+        ScoreText.text = "Score: " + CurrentPlayer.GetComponent<CurrentPlayer>().Score.ToString();
+
+    }
+
+    public void Add10Points()
+    {
+        CurrentPlayer.GetComponent<CurrentPlayer>().Score += 10;
+        UpdateScoreText();
+    }
+
+    public void Add100Points()
+    {
+        CurrentPlayer.GetComponent<CurrentPlayer>().Score += 100;
+        UpdateScoreText();
+    }
+
+    public void UpdateScoreText()
+    {
+        ScoreText.text = "Score: " + CurrentPlayer.GetComponent <CurrentPlayer>().Score.ToString();
+    }
+
+    public void Endgame()
+    {
+        StartCoroutine(SavePlayerScore());
+    }
+
+    IEnumerator SavePlayerScore()
+    {
+        string username = CurrentPlayer.GetComponent<CurrentPlayer>().Username;
+        string scoreFromPlayer = CurrentPlayer.GetComponent<CurrentPlayer>().Score.ToString();
+        WWWForm scoreForm = new WWWForm();
+        scoreForm.AddField("apppassword", "thisisfromtheapp");
+        scoreForm.AddField("username", username);
+        scoreForm.AddField("score", scoreFromPlayer);
+        UnityWebRequest updatePlayerRequest = UnityWebRequest.Post("http://localhost/cruds/updateplayerscore.php", scoreForm);
+        yield return updatePlayerRequest.SendWebRequest();
+        if(updatePlayerRequest.error == null)
+        {
+            
+            string result = updatePlayerRequest.downloadHandler.text;
+            Debug.Log(result);
+            if(result == "0")
+            {
+                FindObjectOfType<SceneSwitch>().LoadGameScene();
+            }
+            else
+            {
+                Debug.Log("error");
+            }
+        }
+        else
+        {
+            Debug.Log(updatePlayerRequest.error);
+        }
+
+    }
+}
---
+++ Portugal Language Learning Game/Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void LoadWelcomeScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNewUserScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadLoginUserScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void LoadPlayGameScene()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+   
+
+  
+
+
+}
