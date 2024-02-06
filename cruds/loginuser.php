913e4f7c513603fd1011f524e5779c45c752edbd
Akshali Jain <jainakshali03@gmail.com>
2024-02-06 12:25:11 +0530

    php files

---
+++ cruds/loginuser.php
@@ -0,0 +1,56 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', 'root', 'course');
+
+if(mysqli_connect_errno())
+{
+    echo("1");
+    exit();
+}
+
+$appkey = $_POST["apppassword"];
+if($appkey != "thisisfromtheapp")
+{
+    exit();
+}
+
+$username = $_POST["username"];
+$usernameClean = filter_var($username, FILTER_SANITIZE_EMAIL);
+$password = $_POST["password"];
+
+$usernamecheckquery = "SELECT * FROM `player` WHERE username = '". $usernameClean ."';";
+$usernamecheckresult = mysqli_query($con, $usernamecheckquery) or die("2");
+
+if($usernamecheckresult->num_rows !=1)
+{
+    echo("3");
+    exit();
+}
+else
+{
+    $fetchedpassword = mysqli_fetch_assoc($usernamecheckresult)["password"];
+    if(password_verify(($password), $fetchedpassword))
+    {
+        $playerInfo = "SELECT * FROM `player` WHERE username = '". $usernameClean ."';";
+        $playerInfoResult = mysqli_query($con, $playerInfo) or die("5");
+        $existingPlayerInfo = mysqli_fetch_assoc($playerInfoResult);
+        $playerUsername = $existingPlayerInfo["username"];
+        $playerScore = $existingPlayerInfo["score"];
+        echo($playerUsername . ":" . $playerScore);
+    }
+    else
+    {
+        echo("4");
+    }
+}
+
+$con->close();
+
+//Error codes
+//1 - Database connection error
+//2 - usernamequery run into an error
+//3 - Username not existing or there is more than 1 in the table
+//4 - Password was not able to be verified
+//5 - PlayerInfo Query Failed
+
+?>
---
+++ cruds/updateplayerscore.php
@@ -0,0 +1,45 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', 'root', 'course');
+
+if(mysqli_connect_errno())
+{
+    echo("1");
+    exit();
+}
+
+$appkey = $_POST["apppassword"];
+
+if($appkey != "thisisfromtheapp")
+{
+    exit();
+}
+
+$username = $_POST["username"];
+$score = $_POST["score"];
+
+$usernamecheckquery = "SELECT * FROM `player` WHERE username = '". $username ."';";
+$usernamecheckresult = mysqli_query($con, $usernamecheckquery) or die("2");
+
+if($usernamecheckresult->num_rows !=1)
+{
+    echo("3");
+    exit();
+}
+
+$updateuserquery = "UPDATE `player` SET score = '".$score."' WHERE username = '". $username ."';";
+mysqli_query($con, $updateuserquery) or die("4");
+
+echo("0");
+
+$con->close();
+
+
+//Error codes
+//0 - Sucess
+//1 - Database connection error
+//2 - usernamecheckquery run into an error
+//3 - User already exists
+//4 - update check query failed
+
+?>
