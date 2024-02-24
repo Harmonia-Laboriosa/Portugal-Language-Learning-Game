7697a4f15875a9e0efc39b3bac60320813c50c35
Chitransh Nishad <91079931+madcritter20789@users.noreply.github.com>
2024-02-24 19:45:26 +0530

    24 Feburary New project

---
+++ Portugal Language Learning Game/Assets/Scripts/PlayerGame.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using TMPro;
+
+public class PlayerGame : MonoBehaviour
+{
+    public TMP_Text UserInfoText;
+    int CurrentPlayerScore;
+    private void Start()
+    {
+        var CurrentPlayer = GameObject.FindGameObjectWithTag("CurrentPlayer");
+        string CurrentPlayerUsername = CurrentPlayer.GetComponent<CurrentPlayer>().Username;
+        CurrentPlayerScore = CurrentPlayer.GetComponent<CurrentPlayer>().Score;
+
+        UserInfoText.text = "User: " + CurrentPlayerUsername + " | " + "Score: " + CurrentPlayerScore.ToString();
+    }
+
+    public void StartGame()
+    {  
+
+        FindObjectOfType<SceneSwitch>().LoadPlayGameScene();
+    }
+    public void RestartGame()
+    {
+        var CurrentPlayer = GameObject.FindGameObjectWithTag("CurrentPlayer");
+       
+        CurrentPlayer.GetComponent<CurrentPlayer>().Score =0;
+        CurrentPlayerScore = CurrentPlayer.GetComponent<CurrentPlayer>().Score;
+
+        FindObjectOfType<SceneSwitch>().LoadPlayGameScene();
+
+
+    }
+  
+    public void SignOut()
+    {
+        var CurrentPlayers = GameObject.FindGameObjectsWithTag("CurrentPlayer");
+        foreach (var item in CurrentPlayers)
+        {
+            Destroy(item);
+        }
+        FindObjectOfType<SceneSwitch>().LoadWelcomeScene();
+    }
+}
