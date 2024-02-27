9b6c6af7d5eca463708ee30bbf4677df41eebf57
Chitransh Nishad <91079931+madcritter20789@users.noreply.github.com>
2024-02-27 15:55:50 +0530

    Timer Managament stop bug fix

--- Portugal Language Learning Game/Assets/Scripts/Level1/Timer.cs
+++ Portugal Language Learning Game/Assets/Scripts/Level1/Timer.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -9,12 +8,19 @@ public class Timer : MonoBehaviour
     [SerializeField] TMP_Text timerText;
     [SerializeField] float totalRemainingTime = 300f; // Total time limit in seconds (5 minutes)
     [SerializeField] GameObject failedPanel;
+    [SerializeField] GameObject endPanel;
 
     private Coroutine blinkCoroutine;
 
     void Update()
     {
-        if (totalRemainingTime < 0)
+        if (failedPanel.activeSelf || endPanel.activeSelf)
+        {
+            // If either panel is active, stop updating the timer
+            return;
+        }
+
+        if (totalRemainingTime <= 0)
         {
             // If time runs out, activate the failed panel
             totalRemainingTime = 0;
