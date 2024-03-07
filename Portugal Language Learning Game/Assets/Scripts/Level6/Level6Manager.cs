1ac02abf1d88742c18fe490456ade0b0487a61e8
Chitransh Nishad <91079931+madcritter20789@users.noreply.github.com>
2024-03-07 22:25:45 +0530

    Level 6 part 1 and 3 logic

---
+++ Portugal Language Learning Game/Assets/Scripts/Level6/Level6Manager.cs
@@ -0,0 +1,141 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Level6Manager : MonoBehaviour
+{
+    public GameObject[] levels;
+    public GameObject EndgamePanel;
+    public GameObject FailedPanel;
+    public TMP_Text scoreText;
+    public Button[] answerButtons; // Changed to an array of buttons
+    private int currentQuestion;
+
+    void Start()
+    {
+        StartQuiz();
+        Debug.Log(levels.Length);
+    }
+
+    void StartQuiz()
+    {
+        // Reset the score when starting the quiz
+        SManage.instance.ResetScore();
+        currentQuestion = 0;
+        ActivateCurrentQuestion();
+        //StartTimer();
+    }
+
+    void ActivateCurrentQuestion()
+    {
+        for (int i = 0; i < levels.Length; i++)
+        {
+            levels[i].SetActive(i == currentQuestion);
+        }
+        EnableAnswerButtons();
+    }
+
+    public void NextQuestion()
+    {
+        if (currentQuestion + 1 < levels.Length)
+        {
+            currentQuestion++;
+
+            ActivateCurrentQuestion();
+        }
+        else
+        {
+            Debug.Log("Quiz completed!");
+            // Display end game panel
+            EndgamePanel.SetActive(true);
+        }
+        EnableAnswerButtons();
+    }
+
+
+
+    public void CorrectAnswer(int correctButtonIndex)
+    {
+        //Destroy(shatter);
+        SManage.instance.IncreaseScore(1);
+        UpdateScoreText();
+        // Shatter all answer buttons after an answer is selected
+        StartCoroutine(DelayBeforeNextQuestion());
+    }
+
+    public void IncorrectAnswer(int correctButtonIndex)
+    {
+        //FailedPanel.SetActive(false);
+        /*
+        Button selectedButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+
+        // Shake the selected button
+        //StartCoroutine(ShakeButton(selectedButton.gameObject, 1f, 20f));
+
+        // Disable all answer buttons after an answer is selected
+        // DisableAnswerButtons();
+        */
+        // Pause the game and show the pause panel
+        StartCoroutine(DelayBeforeNextQuestion());
+        
+        //DisableAnswerButtons();
+    }
+
+    IEnumerator ShakeButton(GameObject buttonObject, float duration, float magnitude)
+    {
+        Vector3 originalPosition = buttonObject.transform.position;
+        float elapsed = 0.0f;
+
+        while (elapsed < duration)
+        {
+            float x = originalPosition.x + Random.Range(-1f, 1f) * magnitude;
+            /*float y = originalPosition.y + Random.Range(-1f, 1f) * magnitude;*/
+
+            buttonObject.transform.position = new Vector3(x, originalPosition.y, originalPosition.z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        buttonObject.transform.position = originalPosition;
+    }
+
+    void UpdateScoreText()
+    {
+        // Update the score text using ScoreManager
+        scoreText.text = "Score: " + SManage.instance.GetScore().ToString();
+    }
+
+    IEnumerator DelayBeforeNextQuestion()
+    {
+        DisableAnswerButtons();
+        // Delay for a short time before moving to the next question
+        yield return new WaitForSeconds(1f);
+        // Disable all buttons during the delay
+
+
+        NextQuestion();
+    }
+
+    void DisableAnswerButtons()
+    {
+        foreach (Button button in answerButtons)
+        {
+            button.interactable = false;
+        }
+    }
+
+    void EnableAnswerButtons()
+    {
+        foreach (Button button in answerButtons)
+        {
+            button.interactable = true;
+        }
+    }
+
+}
