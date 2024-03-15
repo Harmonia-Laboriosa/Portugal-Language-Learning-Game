298f44bf10c70380f76aa90af38fbdafacd7b19c
Akshali Jain <akshali@harmonialaboriosa.com>
2024-03-15 13:05:44 +0530

    level 7 changes

--- Portugal Language Learning Game/Assets/Scripts/Level7/Level7Manager.cs
+++ Portugal Language Learning Game/Assets/Scripts/Level7/Level7Manager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -67,7 +67,7 @@ public class Level7Manager : MonoBehaviour
     public void IncorrectAnswer(int correctButtonIndex)
     {
         //FailedPanel.SetActive(false);
-        /*
+       
         Button selectedButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
 
         // Shake the selected button
@@ -75,9 +75,26 @@ public class Level7Manager : MonoBehaviour
 
         // Disable all answer buttons after an answer is selected
         // DisableAnswerButtons();
-        */
+       
         // Pause the game and show the pause panel
-        StartCoroutine(DelayBeforeNextQuestion());
+
+        int clickedButtonIndex = -1;
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            if (EventSystem.current.currentSelectedGameObject == answerButtons[i].gameObject)
+            {
+                clickedButtonIndex = i;
+                break;
+            }
+        }
+
+        // Disable the clicked incorrect button
+        if (clickedButtonIndex != correctButtonIndex)
+        {
+            answerButtons[clickedButtonIndex].gameObject.SetActive(false);
+        }
+
+        //(DelayBeforeNextQuestion());
 
         //DisableAnswerButtons();
     }
@@ -90,7 +107,7 @@ public class Level7Manager : MonoBehaviour
         while (elapsed < duration)
         {
             float x = originalPosition.x + Random.Range(-1f, 1f) * magnitude;
-            /*float y = originalPosition.y + Random.Range(-1f, 1f) * magnitude;*/
+            *//*float y = originalPosition.y + Random.Range(-1f, 1f) * magnitude;*//*
 
             buttonObject.transform.position = new Vector3(x, originalPosition.y, originalPosition.z);
 
@@ -117,6 +134,140 @@ public class Level7Manager : MonoBehaviour
 
 
         NextQuestion();
+    }
+
+    void DisableAnswerButtons()
+    {
+        foreach (Button button in answerButtons)
+        {
+            button.interactable = false;
+        }
+    }
+
+    void EnableAnswerButtons()
+    {
+        foreach (Button button in answerButtons)
+        {
+            button.interactable = true;
+        }
+    }
+}
+
+
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class Level7Manager : MonoBehaviour
+{
+    public GameObject[] levels;
+    public GameObject EndgamePanel;
+    public GameObject FailedPanel;
+    public TMP_Text scoreText;
+    public Button[] answerButtons;
+    private int currentQuestion;
+
+    // List to keep track of correct answers for each question
+    private List<int> correctAnswerIndices = new List<int>();
+
+    void Start()
+    {
+        StartQuiz();
+        Debug.Log(levels.Length);
+    }
+
+    void StartQuiz()
+    {
+        // Reset the score when starting the quiz
+        SManage.instance.ResetScore();
+        currentQuestion = 0;
+        ActivateCurrentQuestion();
+    }
+
+    void ActivateCurrentQuestion()
+    {
+        for (int i = 0; i < levels.Length; i++)
+        {
+            levels[i].SetActive(i == currentQuestion);
+        }
+        EnableAnswerButtons();
+    }
+
+    public void NextQuestion()
+    {
+        if (currentQuestion + 1 < levels.Length)
+        {
+            currentQuestion++;
+            ActivateCurrentQuestion();
+        }
+        else
+        {
+            Debug.Log("Quiz completed!");
+            // Display end game panel
+            EndgamePanel.SetActive(true);
+        }
+        EnableAnswerButtons();
+    }
+
+    public void CorrectAnswer(int correctButtonIndex)
+    {
+        // Change color of the correct answer button to green
+        answerButtons[correctButtonIndex].image.color = Color.green;
+
+        // Add the selected correct answer index to the list
+        correctAnswerIndices.Add(correctButtonIndex);
+
+        // Check if all correct answers are selected
+        if (correctAnswerIndices.Count == 5) // Assuming there are 5 correct answers
+        {
+            // Increase score only when all correct answers are selected
+            SManage.instance.IncreaseScore(1);
+            UpdateScoreText();
+            StartCoroutine(DelayBeforeNextQuestion());
+        }
+    }
+
+    public void IncorrectAnswer()
+    {
+        // Disable the clicked incorrect button
+        int clickedButtonIndex = GetClickedButtonIndex();
+        
+       
+            answerButtons[clickedButtonIndex].gameObject.SetActive(false);
+        
+    }
+
+    int GetClickedButtonIndex()
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            if (EventSystem.current.currentSelectedGameObject == answerButtons[i].gameObject)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    IEnumerator DelayBeforeNextQuestion()
+    {
+        DisableAnswerButtons();
+        // Delay for a short time before moving to the next question
+        yield return new WaitForSeconds(1f);
+        // Disable all buttons during the delay
+        NextQuestion();
+    }
+
+    void UpdateScoreText()
+    {
+        // Update the score text using ScoreManager
+        scoreText.text = "Score: " + SManage.instance.GetScore().ToString();
     }
 
     void DisableAnswerButtons()
