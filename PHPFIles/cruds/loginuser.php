f1fcbda0cb42fb87d23f56ed2500721133bbd647
Chitransh Nishad <91079931+madcritter20789@users.noreply.github.com>
2024-05-02 19:33:57 +0530

    Level 6to8 Question check

--- PHPFIles/cruds/loginuser.php
+++ PHPFIles/cruds/loginuser.php
@@ -52,5 +52,23 @@ $con->close();
 //3 - Username not existing or there is more than 1 in the table
 //4 - Password was not able to be verified
 //5 - PlayerInfo Query Failed
+// Allow from any origin
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
+
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    exit(0);
+}
 
 ?>
